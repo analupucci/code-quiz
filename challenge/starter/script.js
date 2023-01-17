3350d22ff9552ba6f525940a4d8913e81eafa150
analupucci <analupucci@gmail.com>
2023-01-17 18:31:37 +0000

    added three questions to the quiz

---
+++ challenge/starter/script.js
@@ -0,0 +1,31 @@
+// Quiz questions and answers
+const questions = [
+    {
+      question: "What is the difference between let and var in JavaScript?",
+      answers: {
+        a. "let is block-scoped while var is function-scoped"
+        b. "let is hoisted while var is not"
+        c. "let is used for declaring constants while var is used for declaring variables"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is the function of the 'this' keyword in JavaScript?",
+      answers: {
+        a. "It refers to the object that is calling the function"
+        b. "It refers to the parent object of the function"
+        c. "It refers to the global object"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: "What is the difference between == and === in JavaScript?",
+      answers: {
+        a. "== compares values only, while === compares both values and types"
+        b. "== is used for comparing numbers, while === is used for comparing strings"
+        c. "== is a new feature in ECMAScript 6, while === has always been available in JavaScript"
+      },
+      correctAnswer: "a"
+    }
+  ];
+  
